42e6fee345ba838a3bf33b9cf9995383bf7b5eca
kabarokore jesca <93138670+kabarokore@users.noreply.github.com>
2024-11-03 18:42:17 +0200

    Add files via upload

---
+++ src/main/java/net/enjoy/springboot/auditingmanagentsystem/AuditingManagentSystemApplication.java
@@ -0,0 +1,12 @@
+package net.enjoy.springboot.auditingmanagentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditingManagentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuditingManagentSystemApplication.class, args);
+    }
+}
---
+++ src/main/java/net/enjoy/springboot/auditingmanagentsystem/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package net.enjoy.springboot.auditingmanagentsystem.repository;
+
+import net.enjoy.springboot.auditingmanagentsystem.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
